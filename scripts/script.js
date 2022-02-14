d9bebf2536e3abb7692657a819cc78293d8851d8
Wesley Ting <wesleyting3@gmail.com>
2022-02-14 11:38:48 -0800

    Preparation for Work pages and minor optimizaitons

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,30 @@
-const body = document.body;
+const wrapper = document.querySelector('.wrapper');
 const btn = document.querySelector('.hamburger');
 
 btn.addEventListener('click', function(){
-    body.classList.toggle('show');
+    wrapper.classList.toggle('show');
 });
 
 function checkWidth() {
     if ($(window).width() < 950) {
-        $('#body').removeClass('show');
+        $('.wrapper').removeClass('show');
     }
 }
 
 $(window).resize(checkWidth);
 
 
+var hideTOC = function(){
+    var hideThese = document.getElementsByClassName("toc_list");
+    for (var i = 0; i < hideThese.length; i++){
+        hideThese[i].style.display = "none";
+    }
+    document.getElementById("showTOC").style.display = "";
+}
+var showTOC = function(){
+    var showThese = document.getElementsByClassName("toc_list");
+    for (var i = 0; i < showThese.length; i++){
+        showThese[i].style.display = "";
+    }
+    document.getElementById("showTOC").style.display = "none";
+}
