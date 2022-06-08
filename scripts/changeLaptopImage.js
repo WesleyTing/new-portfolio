1614e18c33157e18416e3cd02503a31c56f1f7ea
Wesley Ting <wesleyting3@gmail.com>
2022-06-08 15:41:47 -0700

    Added Images, Lightbox, and Prism

---
+++ scripts/changeLaptopImage.js
@@ -0,0 +1,9 @@
+function before(){
+    document.getElementById('laptop-image')
+    .src="../images/work/token/laptop-content-2.jpg";
+}
+ 
+function after(){
+    document.getElementById('laptop-image')
+    .src="../images/work/token/laptop-content.jpg";
+}
