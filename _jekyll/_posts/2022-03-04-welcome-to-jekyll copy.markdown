fe1026664b91d1c9e4bb39fec64cf750393b93c9
Wesley Ting <wesleyting3@gmail.com>
2023-04-06 13:57:54 -0700

    New pages, videos, and updates
    Pages added:
    treasuremart
    fullcircle
    statcblogr
    
    Bl;og updated

--- _jekyll/_posts/2022-03-04-welcome-to-jekyll copy.markdown
+++ _jekyll/_posts/2022-03-04-welcome-to-jekyll copy.markdown
@@ -1,9 +1,9 @@
 ---
 layout: post
-title:  "The World and Its Qualms"
-date:   2022-02-26 08:00:00 -0800
+title: "TBD"
+date: 2022-02-26 08:00:00 -0800
 categories: blog
-summary: As the world gets smaller for our growing population, our problems grow bigger.
+summary:
 ---
 
 To be written...
--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,29 @@
-const wrapper = document.querySelector('.wrapper');
-const btn = document.querySelector('.hamburger');
+const wrapper = document.querySelector(".wrapper");
+const btn = document.querySelector(".hamburger");
 
-btn.addEventListener('click', function(){
-    wrapper.classList.toggle('show');
+btn.addEventListener("click", function () {
+  wrapper.classList.toggle("show");
 });
 
 function checkWidth() {
-    if ($(window).width() < 950) {
-        $('.wrapper').removeClass('show');
-    }
+  if ($(window).width() < 950) {
+    $(".wrapper").removeClass("show");
+  }
 }
 
 $(window).resize(checkWidth);
 
-
-var hideTOC = function(){
-    var hideThese = document.getElementsByClassName("toc_list");
-    for (var i = 0; i < hideThese.length; i++){
-        hideThese[i].style.display = "none";
-    }
-    document.getElementById("showTOC").style.display = "";
-}
-var showTOC = function(){
-    var showThese = document.getElementsByClassName("toc_list");
-    for (var i = 0; i < showThese.length; i++){
-        showThese[i].style.display = "";
-    }
-    document.getElementById("showTOC").style.display = "none";
-}
+var hideTOC = function () {
+  var hideThese = document.getElementsByClassName("toc_list");
+  for (var i = 0; i < hideThese.length; i++) {
+    hideThese[i].style.display = "none";
+  }
+  document.getElementById("showTOC").style.display = "";
+};
+var showTOC = function () {
+  var showThese = document.getElementsByClassName("toc_list");
+  for (var i = 0; i < showThese.length; i++) {
+    showThese[i].style.display = "";
+  }
+  document.getElementById("showTOC").style.display = "none";
+};
